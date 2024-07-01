8ec36c0fefbe646f7d103f94226503c7e3302a93
Neerajnsutian <neerajofficial1919@gmail.com>
2024-07-02 02:28:26 +0530

    final changes

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd20dd37e-b0b8-4d2a-92d6-d84066ce202b';
+export const MODAL_CLOSE_SEC = 2.5;
